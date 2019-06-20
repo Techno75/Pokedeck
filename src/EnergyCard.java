6ceaa33b0ca08cd34e3fc80766a13d8d0027f6b9
Guillaume Depretz <gdepretz@gmail.com>
2019-06-20 23:40:41 +0200

    Update EnergyCard.java

--- src/EnergyCard.java
+++ src/EnergyCard.java
@@ -4,12 +4,6 @@ public class EnergyCard extends Card {
 	private String element;
 	public static String Elements[] = {"grass", "fire", "water", "lighting", "psychic", "fighting", "darkness", "metal", "fairy", "dragon", "colorless" };
 	
-	/**
-	 * 
-	 * @param name
-	 * @param type
-	 * @param element
-	 */
 	public EnergyCard(String name, String type, String element) {
 		super(name, type);
 		this.element = element;
