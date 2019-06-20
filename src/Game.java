a7d283c8e9684aff9b5ed8ec913f573af5968342
Guillaume Depretz <gdepretz@gmail.com>
2019-06-20 23:41:14 +0200

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -7,52 +7,5 @@ public class Game {
 		Player player1 = new Player("Bob");
 		
 		MainPanel panel = new MainPanel();
-		
-		
-		
-//		Scanner userInput = new Scanner(System.in);
-//		System.out.println("Type the first player's name :");
-//		String playerName = userInput.next();
-//		Player player1 = new Player(playerName);
-//		
-//		System.out.println("Type the second player's name :");
-//		playerName = userInput.next();
-//		Player player2 = new Player(playerName);
-//		
-//		System.out.println(player1);
-//		System.out.println(player2);
-//		
-//		System.out.println("Create a card ! ");
-//		System.out.println("Select a player : ");
-//		String player = userInput.next();
-//		
-//		System.out.println("Select the type (Trainer, Pokemon or Energy) : ");
-//		String cardType = userInput.next();
-//		
-//		if(cardType.toLowerCase().equals("trainer")){
-//			System.out.println("Type the name of the card : ");
-//			String cardName = userInput.next();
-//			System.out.println("Type the trainer type : ");
-//			String cardTrainerType = userInput.next();
-//			System.out.println("Type the text of the card : ");
-//			String cardTextBox = userInput.next();
-//			System.out.println("Type the rule of the card : ");
-//			String cardRule = userInput.next();
-//			
-//			Card card1 = new TrainerCard(cardName, "Trainer", cardTrainerType, cardTextBox, cardRule);
-//			
-//			System.out.println(card1);
-//		}
-//		else if (cardType.toLowerCase() == "pokemon") {
-//			
-//		}
-//		else if (cardType.toLowerCase() == "energy") {
-//			
-//		}
-//		else{
-//			System.out.println("You mistyped the type. Launch the program again");
-//		}
-		
-	}
 
 }
