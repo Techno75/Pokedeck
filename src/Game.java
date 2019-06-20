28cada75ff591f3cf72b3f6e1195aab4e3b6a519
Guillaume Depretz <gdepretz@gmail.com>
2019-06-20 23:41:27 +0200

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		Player player1 = new Player("Bob");
 		
 		MainPanel panel = new MainPanel();
